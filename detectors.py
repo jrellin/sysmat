69150a44433dc289fc3325b6bf557af65f341433
Justin Ellin <justinrellin@gmail.com>
2020-03-02 08:59:52 -0800

    Renaming files

--- detectors.py
+++ detectors.py
@@ -1,12 +1,12 @@
 import numpy as np
-
+from scipy.sparse import lil_matrix
 
 #  Global origin (0,0,0) should be in the center of the collimator on the detector-collimator axis
 # This function will eventually handle exact detector orientations
 
 
 class Detector(object):
-    mu = 100.0  # mm^2/g mass attenuation coeffecient
+    mu = 100.0  # mm^2/g mass attenuation coefficient
     rho = 1/1000.0  # density g/mm^3
 
     def __init__(self, center=(0, 0, 100),  # mm Coordinates of the center face of
@@ -26,9 +26,6 @@ class Detector(object):
         self.pix_isodd = self.npix & 0x1
         self.pix_size = np.array(([pix1_size, pix2_size]))
         self.axes = np.array([ax_1, ax_2])
-        self.upper_left = self.c - (self.npix[0]/2 * self.pix_size[0] * self.axes[0]) - \
-                          (self.npix[1]/2 * self.pix_size[1] * self.axes[1])
-        # on face of detector nearest the collimator
 
         self.f_plane = np.dot(self.c, self.norm)
         self.b_plane = np.dot(self.c + ((-1) * self.thickness * self.norm), self.norm)
@@ -53,7 +50,7 @@ class Detector(object):
 
         return centers.reshape(self.npix[0], self.npix[1]) + (back * (-1) * self.thickness * self.norm)
 
-    def crystal_intersections(self, emission_pt, emission_dir, step=0.01, prefactor = 1):  # step in mm
+    def crystal_intersections(self, emission_pt, emission_dir, step=0.1, prefactor=1):  # step in mm
 
         r_o = (self.f_plane - np.dot(emission_pt, self.norm)) / (np.dot(emission_dir, self.norm) * 1.0)
         omegas = np.arange(
@@ -61,41 +58,34 @@ class Detector(object):
             float((self.b_plane - np.dot(emission_pt, self.norm)) / (1.0 * np.dot(emission_dir), self.norm) + step),
             step
         )
-        theta = np.abs(np.dot(np.vstack([self.norm, self.axes]), emission_dir))
-        # theta_f = np.dot(-emission_dir, self.norm)
-        # theta_0 = np.abs(np.dot(emission_dir, self.axes[0]))
-        # theta_1 = np.abs(np.dot(emission_dir, self.axes[1]))
 
-        area = (self.pix_size * np.cos(theta[0]) + self.pix_size[1] * np.cos(theta[1]) +
-                                                           self.pix_size[0] * np.cos(theta[2])) * self.thickness
         ray = np.outer(omegas, emission_dir) + emission_pt
         intersection = (np.abs(np.dot(ray, self.axes[0])) < self.hist_ax0[-1]) & \
                        (np.abs(np.dot(ray, self.axes[1])) < self.hist_ax1[-1])
 
         if not np.any(intersection):
-            return {'total_intersection': 0}
+            return {'total_interaction': 0}
 
-        intersection_rays = ray[intersection] - self.upper_left  # rays from center of face of detector
-        # intersection_rays = ray[intersection]
-        # TODO: Project onto crystal axes
+        intersection_rays = ray[intersection] - self.c  # rays from center of face of detector
 
         omega_cross = omegas[intersection]
         ent_ext = (omega_cross[0], omega_cross[-1])
+        # exit currently unused
 
-        #  solid_angle = area/(4 * np.pi * omega_cross**2)
+        theta = np.abs(np.dot(np.vstack([self.norm, self.axes]), emission_dir))
+        area = (self.pix_size[0] * self.pix_size[1] * np.cos(theta[0])) + \
+               (self.pix_size[1] * self.thickness * np.cos(theta[1])) + \
+               (self.pix_size[0] * self.thickness * np.cos(theta[2]))
 
         prob_interact = np.exp(-self.mu * self.rho * step * np.arange(omega_cross.size)) * \
                         (1-np.exp(-self.mu * self.rho * step)) * (area/(4 * np.pi * omega_cross**2))
 
-        # crystal_ind = np.floor(intersection_rays[:, :2] + 0.5 * self.pix_isodd) \
-        #              + (0.5 * (self.pix_isodd ^ 1)).astype(int)
+        crystal_interactions = np.histogram2d(np.dot(intersection_rays, self.axes[0]),
+                       np.dot(intersection_rays, self.axes[1]),
+                       bins=(self.hist_ax0, self.hist_ax1),
+                       weights=prob_interact)[0]
+        return {'total_interaction': np.sum(prob_interact),
+                'crystal_interaction': lil_matrix(crystal_interactions),
+                'enter': ent_ext[0]}
 
-        crystal_ind = intersection_rays - self.upper_left
-
-        # omegas[intersection][[0, -1]] # First and last value
-
-        una = np.unique(crystal_ind[:, :2], return_counts=True, return_index=True, axis=0)
-
-        # TODO: crystal_ind is wrong (not defined relative to crystal)
         # TODO: This might be possible to do all at once for fixed emission points
-
